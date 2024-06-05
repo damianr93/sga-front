c3e62830bb062cfbe22f2632222b122cc72e9820
Damian <a.damianrodriguez.93@gmail.com>
2024-06-05 12:31:01 -0300

    waste table implemened

--- src/components/collapsed-table/collapsed-table.tsx
+++ src/components/collapsed-table/collapsed-table.tsx
@@ -1,74 +1,149 @@
-import { styled } from '@mui/material/styles';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import styled from 'styled-components';
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
+const StyledTableCell = styled(TableCell)(() => ({
+  '&.MuiTableCell-head': {
+    backgroundColor: 'black',
+    color: 'white',
   },
-  [`&.${tableCellClasses.body}`]: {
+  '&.MuiTableCell-body': {
     fontSize: 14,
+    backgroundColor: 'white',
   },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
+const StyledButton = styled.button`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: black;
+  cursor: pointer;
+`;
+
 
 function createData(
   name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  tipo:string,
+  medicion: number,
+  fecha: string,
+  proximaFecha: string,
 ) {
-  return { name, calories, fat, carbs, protein };
+  return {
+    name,
+    tipo,
+    medicion,
+    fecha,
+    proximaFecha,
+    history: [
+      {
+        date: '2020-01-05',
+        customerId: '11091700',
+        amount: 3,
+      },
+      {
+        date: '2020-01-02',
+        customerId: 'Anonymous',
+        amount: 1,
+      },
+    ],
+  };
+}
+
+function Row(props: { row: ReturnType<typeof createData> }) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <StyledTableCell>
+          <StyledButton
+            aria-label="expand row"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <ArrowDropUpIcon sx={{color:'white'}}/> : <ArrowDropDownIcon />}
+          </StyledButton>
+        </StyledTableCell>
+        <StyledTableCell component="th" scope="row">
+          {row.name}
+        </StyledTableCell>
+        <StyledTableCell align="center">{row.tipo}</StyledTableCell>
+        <StyledTableCell align="center">{row.medicion}</StyledTableCell>
+        <StyledTableCell align="center">{row.fecha}</StyledTableCell>
+        <StyledTableCell align="center">{row.proximaFecha}</StyledTableCell>
+      </TableRow>
+      <TableRow>
+        <StyledTableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 0, backgroundColor: 'black' }}>
+              <Typography variant="h6" gutterBottom component="div" sx={{ backgroundColor: 'white', color: 'black' }}>
+                History
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell>Fecha</StyledTableCell>
+                    <StyledTableCell>Medido por</StyledTableCell>
+                    <StyledTableCell align="right">Medición</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.date}>
+                      <StyledTableCell component="th" scope="row">
+                        {historyRow.date}
+                      </StyledTableCell>
+                      <StyledTableCell>{historyRow.customerId}</StyledTableCell>
+                      <StyledTableCell align="right">{historyRow.amount}</StyledTableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </StyledTableCell>
+      </TableRow>
+    </React.Fragment>
+  );
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Damian', 'Carton', 159, '01-05-2024', '01-06-2024'),
+  createData('Alejandro', 'Generales', 237, '01-04-2024', '01-05-2024'),
+  createData('Luis', 'Especiales', 262, '01-03-2024', '01-04-2024'),
+  createData('Julio', 'Metales', 305, '01-02-2024', '01-03-2024'),
+  createData('Christian', 'Liquidos Especiales', 356, '01-01-2024', '01-02-2024'),
 ];
 
-export default function CustomizedTables() {
+export default function CollapsibleTable() {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+      <Table aria-label="collapsible table">
         <TableHead>
-          <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+          <TableRow >
+            <StyledTableCell />
+            <StyledTableCell>Medido por</StyledTableCell>
+            <StyledTableCell align="center">Tipo</StyledTableCell>
+            <StyledTableCell align="center" >Medición</StyledTableCell>
+            <StyledTableCell align="center">Fecha</StyledTableCell>
+            <StyledTableCell align="right">Próxima medición</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
+            <Row key={row.name} row={row} />
           ))}
         </TableBody>
       </Table>
